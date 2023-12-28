67223ce8886b828bd461057a53e0efddbc7f78d2
JosephOkumu <jokumu25@gmail.com>
2023-12-28 17:20:33 +0300

    Return usage

---
+++ ReturnName/returnaming.go
@@ -0,0 +1,14 @@
+// This program shows how to manipulate and name the the return values.
+package main
+
+import "fmt"
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
