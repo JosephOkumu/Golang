a57064a6b59941b2984c90fb209a37e5da8a4030
JosephOkumu <jokumu25@gmail.com>
2023-12-29 18:39:52 +0300

    shor form of declaring and initializing a variable in go

---
+++ ShortVarNotation/shortvar.go
@@ -0,0 +1,13 @@
+//This program highlights the short form of declaring and intializing a variable.
+
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	j := 2
+	k := 3
+	var c, python, java = true, false, "No!"
+	fmt.Println(i, j, k, c, python, java)
+}
