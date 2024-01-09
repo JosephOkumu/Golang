332ec6a18f664bb2c507ff9285e3d5add170092d
JosephOkumu <jokumu25@gmail.com>
2024-01-09 18:59:31 +0300

    Adding comments

--- DispDigit/dispdigit.go
+++ DispDigit/dispdigit.go
@@ -8,14 +8,18 @@ func dispDigit(n int) {
 		z01.PrintRune('0')
 		return
 	} else if n < 0 {
+		//The code below prints negative.
 		z01.PrintRune('-')
+		//The line of code below makes the negative n above positive.
 		n = -n
 	}
+	//The printNba function in the line below is called using the positive n above
 	printNba(n)
 }
 
 func printNba(n int) {
 	if n/10 != 0 {
+		//The line below calls itself recursively if n/10 != 0
 		printNba(n / 10)
 	}
 	z01.PrintRune('0' + rune(n%10))
