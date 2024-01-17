14f9ac99b7876f34762d2e276e04c7492de52bc3
JosephOkumu <jokumu25@gmail.com>
2024-01-17 21:14:20 +0300

    Building another quad

---
+++ Tetra-D/tetra-d.go
@@ -0,0 +1,38 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func tetraD(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+	for row := 0; row <= y-1; row++ {
+		for col := 0; col <= x-1; col++ {
+			if row == 0 || row == y-1 {
+				if row == 0 && col == 0 {
+					z01.PrintRune('A')
+				} else if row == 0 && col == x-1 {
+					z01.PrintRune('C')
+				} else if row == y-1 && col == 0 {
+					z01.PrintRune('A')
+				} else if row == y-1 && col == x-1 {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			} else if row > 0 || row < y-1 {
+				if col == 0 || col == x-1 {
+					z01.PrintRune('B')
+
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
+
+func main() {
+	tetraD(5, 1)
+}
