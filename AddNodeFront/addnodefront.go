438b01021e6f21c166f09a8f8af1c0dc032030b4
JosephOkumu <jokumu25@gmail.com>
2024-02-11 21:15:07 +0300

    Adding comments

--- AddNodeFront/addnodefront.go
+++ AddNodeFront/addnodefront.go
@@ -2,30 +2,31 @@ package main
 
 import "fmt"
 
+// NodeL is a node in the linked list
 type NodeL struct {
-	data interface{}
-	next *NodeL
+	data interface{} // data of any type
+	next *NodeL      // pointer to the next node
 }
 
+// List is the linked list
 type List struct {
-	head *NodeL
-	tail *NodeL
+	head *NodeL // pointer to the first node
+	tail *NodeL // pointer to the last node
 }
 
+// addNodeFront adds a new node to the front of the list
 func addNodeFront(l *List, data interface{}) {
 	newNode := &NodeL{
 		data: data,
 		next: nil,
 	}
-	if l.head == nil {
-		l.head = newNode
-		l.tail = newNode
+	if l.head == nil { // if the list is empty
+		l.head = newNode // set the new node as the head
+		l.tail = newNode // set the new node as the tail
 	} else {
-		newNode.next = l.head
-		l.head = newNode
-
+		newNode.next = l.head // set the next of the new node to the current head
+		l.head = newNode      // update the head to the new node
 	}
-
 }
 
 func main() {
@@ -41,7 +42,5 @@ func main() {
 	for it != nil {
 		fmt.Print(it.data, " ")
 		it = it.next
-
 	}
-
 }
