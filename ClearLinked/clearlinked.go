19cbabcf3f4234e9a951741598070250cc90394a
JosephOkumu <jokumu25@gmail.com>
2024-02-12 19:14:54 +0300

    Clearing a linked list

---
+++ ClearLinked/clearlinked.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data interface{}
+	next *Node
+}
+
+type List struct {
+	head *Node
+	tail *Node
+}
+
+func addNodEnd(l *List, data interface{}) {
+	newNode := &Node{data: data, next: nil}
+
+	if l.head == nil {
+		l.head = newNode
+		l.tail = newNode
+	} else {
+		l.tail.next = newNode
+		l.tail = newNode
+	}
+}
+
+func clearLinked(l *List) {
+	l.head = nil
+	l.tail = nil
+}
+
+func printList(l *List) {
+	iterator := l.head
+	for iterator != nil {
+		fmt.Print(iterator.data, " -> ")
+		iterator = iterator.next
+	}
+	fmt.Println(nil)
+}
+
+func main() {
+	link := &List{}
+
+	addNodEnd(link, "I")
+	addNodEnd(link, 7)
+	addNodEnd(link, "Christian")
+	addNodEnd(link, 35)
+
+	fmt.Println("------list------")
+	printList(link)
+	clearLinked(link)
+	fmt.Println("------updated list------")
+	printList(link)
+}
